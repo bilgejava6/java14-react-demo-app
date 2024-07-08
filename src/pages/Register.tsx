ae7a9ee5d2455aa502c4132c7ca0d39535025238
bilgejava6 <bilgejava6@gmail.com>
2024-07-08 10:58:12 +0300

    ...

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -18,14 +18,17 @@ function Register() {
   return (
     <>
     <div className="container">
-        <div className="row justify-content-center mt-5">
+        <div className="row justify-content-center mt-5">            
             <div className="col-4">
+              <div className='mb-5 text-center'>
+                <h1>Register Page</h1>
+              </div>
                 {
                     isEmpty ?
                     <div className="alert alert-warning" role="alert">
                         <h4 className="alert-heading">Uyarı</h4>                            
                         <hr />
-                        <p className="mb-0">Kullanıcı adı ve Şifre boş geçilemez</p>
+                        <p className="mb-0">Kullanıcı adı, Şifre ve email boş geçilemez</p>
                     </div>
                     : null
                 }
@@ -37,6 +40,19 @@ function Register() {
                 <div className="mb-3">
                     <label htmlFor="password" className="form-label">Password</label>
                     <input onChange={evt=>setPassword(evt.target.value)} type="password" className="form-control" id="password" placeholder="password" />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="repassword" className="form-label">rePassword</label>
+                    <input onChange={evt=>setRePassword(evt.target.value)} type="password" className="form-control" id="repassword" placeholder="repassword" />
+                    {
+                      password !== rePassword 
+                      ? <div className="text-danger mt-1"> * Şifreler uyuşmuyor.</div>
+                      : null
+                    }
+                </div>
+                <div className="mb-5">
+                  <label htmlFor="email" className="form-label">Email</label>
+                  <input onChange={evt=>setEmail(evt.target.value)} type="email" className="form-control" placeholder='email'/>
                 </div>
                 <div className="mb-3 d-grid">
                     <input onClick={register} type="button" value="Register" className="btn btn-success" />    
